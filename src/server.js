8936239d3651c2873b172978afd5ccfdfae0724d
VanHoa2k2 <91736384+VanHoa2k2@users.noreply.github.com>
2023-06-27 14:30:01 +0700

    updates

---
+++ src/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+import  express from "express";
+import viewEngine from "./configs/viewEngine";
+import webRoutes from "./routes/web";
+import bodyParser from "body-parser";
+// import initCronJob from "./config/cronJob";
+
+let app = express();
+
+//use body-parser to post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//config view engine
+viewEngine(app);
+
+// init all web routes
+webRoutes(app);
+
+//init cron job
+// initCronJob();
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+   console.log(`App is running at the port ${port}`);
+});
