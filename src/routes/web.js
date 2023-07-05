f5d83ff555e74e19ed816ccab5856f93d015d5fb
VanHoa2k2 <91736384+VanHoa2k2@users.noreply.github.com>
2023-07-05 21:52:07 +0700

    u

--- src/routes/web.js
+++ src/routes/web.js
@@ -16,7 +16,7 @@ let initWebRoutes = (app) => {
     router.get("/webhook", homeController.getWebhook);
 
     router.get("/reserve-table/:senderId", homeController.handleReserveTable);
-    router.get("/reserve-table-ajax", homeController.handlePostReserveTable);
+    router.post("/reserve-table-ajax", homeController.handlePostReserveTable);
 
 
     return app.use("/", router);
